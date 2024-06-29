a6262cd9b2787025daab4188725d9ad3b1e4aa50
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-29 12:44:04 +0530

    set routes and components and create code editor page

---
+++ client/src/components/CodeEditor.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { tags as t } from "@lezer/highlight";
+import { draculaInit } from "@uiw/codemirror-theme-dracula";
+import {
+  loadLanguage,
+  langNames,
+  langs,
+} from "@uiw/codemirror-extensions-langs";
+
+export default function CodeEditor() {
+  const [value, setValue] = React.useState("console.log('hello world!');");
+  const onChange = React.useCallback((val: any) => {
+    console.log("val:", val);
+    setValue(val);
+  }, []);
+  return (
+    <CodeMirror
+      value={value}
+      height="100vh"
+      extensions={[loadLanguage("javascript")!]}
+      onChange={onChange}
+      theme={draculaInit({
+        settings: {
+          caret: "#c6c6c6",
+          fontFamily: "monospace",
+        },
+        styles: [{ tag: t.comment, color: "#6272a4" }],
+      })}
+    />
+  );
+}
---
+++ client/src/components/HelperHeader.tsx
@@ -0,0 +1,45 @@
+import { Share2, Save } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function HelperHeader() {
+  return (
+    <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
+      <div className="__btn_container flex gap-1">
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="success"
+        >
+          <Save size={16} />
+          Save
+        </Button>
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="secondary"
+        >
+          <Share2 size={16} />
+          Share
+        </Button>
+      </div>
+      <div className="__tab_switcher flex justify-center items-center gap-1">
+        <small>Current Language:</small>
+        <Select defaultValue="html">
+          <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="html">HTML</SelectItem>
+            <SelectItem value="css">CSS</SelectItem>
+            <SelectItem value="javascript">JavaScript</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
+}
