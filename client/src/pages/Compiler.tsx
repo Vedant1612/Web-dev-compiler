5591f18388d2f050dd62095bf8ed51152970f524
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-30 16:44:17 +0530

    add load-code api with frontend

--- client/src/pages/Compiler.tsx
+++ client/src/pages/Compiler.tsx
@@ -6,34 +6,49 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { handleError } from "@/utils/handleError";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { updateFullCode } from "@/redux/slices/compilerSlice";
 
 export default function Compiler() {
-  
   const { urlId } = useParams();
-  useEffect(()=>{
-    if(urlId){
-      
+  const dispatch = useDispatch();
+
+  const loadCode = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/compiler/load", {
+        urlId: urlId,
+      });
+      dispatch(updateFullCode(response.data.fullCode));
+    } catch (error) {
+      handleError(error);
     }
-  },[urlId])
-  
+  };
+
+  useEffect(() => {
+    if (urlId) {
+      loadCode();
+    }
+  }, [urlId]);
+
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel
         className="h-[calc(100dvh-60px)] min-w-[350px]"
         defaultSize={50}
       >
-        <HelperHeader/>
-        <CodeEditor/>
+        <HelperHeader />
+        <CodeEditor />
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel
         className="h-[calc(100dvh-60px)] min-w-[350px]"
         defaultSize={50}
       >
-        <RenderCode/>
+        <RenderCode />
       </ResizablePanel>
     </ResizablePanelGroup>
   );
