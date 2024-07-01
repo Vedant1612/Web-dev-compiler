b5c3ac8c15fc83efad78c5fc28e59708ebf91ada
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-01 18:43:17 +0530

    create login page UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,21 @@ import Compiler from "./pages/Compiler";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "./components/ui/sonner";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
-      <Toaster position="bottom-right" theme="dark"/>
+      <Toaster position="bottom-right" theme="dark" />
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/compiler" element={<Compiler />} />
-          <Route path="/compiler/:urlId" element={<Compiler />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/compiler" element={<Compiler />} /> */}
+          <Route path="/compiler/:urlId?" element={<Compiler />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ThemeProvider>
