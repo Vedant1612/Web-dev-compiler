981894ac0f2faa479678a614b557175fa0d12558
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-01 15:50:31 +0530

    Setup Authentication in Backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,14 @@ const app = express();
 import { config } from "dotenv";
 import { dbConnect } from "./lib/dbConnect";
 import { compilerRoutes } from "./routes/compilerRoutes";
+import { userRouter } from "./routes/userRoutes";
 
 app.use(express.json());
 app.use(cors());
 config();
 
 app.use('/compiler', compilerRoutes)
+app.use('/user', userRouter)
 
 dbConnect();
 app.listen(4000, () => {
