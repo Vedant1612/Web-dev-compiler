b3aec8d1f6b9e39b009e724b079064511f763026
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-03 15:21:53 +0530

    connect logout API with frontend

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,27 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { handleError } from "@/utils/handleError";
+import { useLogoutMutation } from "@/redux/slices/api";
+import { updateCurrentUser, updateIsLoggedIn } from "@/redux/slices/appSlice";
 
 export default function Header() {
+  const [logout, { isLoading }] = useLogoutMutation();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.appSlice.isLoggedIn
+  );
+
+  async function handleLogout() {
+    try {
+      await logout().unwrap();
+      dispatch(updateIsLoggedIn(false));
+      dispatch(updateCurrentUser({}));
+    } catch (error) {
+      handleError(error);
+    }
+  }
   return (
     <nav className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
       <Link to="/">
@@ -13,16 +33,32 @@ export default function Header() {
             <Button variant="secondary">Compiler</Button>
           </Link>
         </li>
-        <li>
-          <Link to="/login">
-            <Button variant="blue">Login</Button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/signup">
-            <Button variant="blue">Signup</Button>
-          </Link>
-        </li>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <Button
+                loading={isLoading}
+                onClick={handleLogout}
+                variant="destructive"
+              >
+                Logout
+              </Button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <Button variant="blue">Login</Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup">
+                <Button variant="blue">Signup</Button>
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
