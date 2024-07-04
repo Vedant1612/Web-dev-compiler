4149cf87be2fee5e918958d88b946ab79c95147e
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-04 20:39:28 +0530

    minor changes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,8 @@
+// import "./bg/bg.css"
 export default function Home() {
   return (
     <div className="w-full h-[calc(100dvh-60px)] text-white flex justify-center items-center flex-col gap-3">
-      <h1 className="text-2xl md:text-6xl font-bold text-center">WD Compiler</h1>
+      <h1 className="text-2xl md:text-6xl font-bold text-center">Web Dev Compiler</h1>
       <p className=" text-gray-500 text-center">
         Compiler HTML, CSS, JavaScript Code on the go and share it with your
         friends
