f095ea53ba410938f38b1bfc2583904f7750d395
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-04 12:55:26 +0530

    Add delete functionality

--- server/src/routes/compilerRoutes.ts
+++ server/src/routes/compilerRoutes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { loadCode, saveCode } from "../controllers/compilerController";
+import {
+  deleteCode,
+  editCode,
+  loadCode,
+  saveCode,
+} from "../controllers/compilerController";
 import { verifyTokenAnonymous } from "../middlewares/verifyTokenAnonymous";
+import { verifyToken } from "../middlewares/verifyToken";
 
 export const compilerRoutes = express.Router();
 
-compilerRoutes.post("/save",verifyTokenAnonymous, saveCode);
-
+compilerRoutes.post("/save", verifyTokenAnonymous, saveCode);
+compilerRoutes.delete("/delete/:id", verifyToken, deleteCode);
 compilerRoutes.post("/load", loadCode);
+compilerRoutes.put("/edit", verifyToken, editCode);
