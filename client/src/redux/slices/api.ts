c687a9f9d690c379e5b48d89c24ce860593b30f7
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-08 15:42:00 +0530

    handle cors error for deployment

--- client/src/redux/slices/api.ts
+++ client/src/redux/slices/api.ts
@@ -9,7 +9,7 @@ import {
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000",
+    baseUrl: "https://web-dev-compiler.onrender.com",
     credentials: "include",
   }),
   tagTypes: ["myCodes", "allCodes"],
