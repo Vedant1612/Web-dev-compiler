6992cd05d662e2fd1cbc11fc6f2a0b369aab1acb
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-08 15:30:08 +0530

    changes for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,17 +5,18 @@ import { config } from "dotenv";
 import { dbConnect } from "./lib/dbConnect";
 import { compilerRoutes } from "./routes/compilerRoutes";
 import { userRouter } from "./routes/userRoutes";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 app.use(express.json());
-app.use(cookieParser())
-app.use(cors({credentials:true,origin:"http://localhost:5173"}));
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 config();
 
-app.use('/compiler', compilerRoutes)
-app.use('/user', userRouter)
+app.use("/compiler", compilerRoutes);
+app.use("/user", userRouter);
 
 dbConnect();
-app.listen(4000, () => {
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
   console.log(`http://localhost:4000`);
 });
