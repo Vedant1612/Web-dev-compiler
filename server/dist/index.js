8b5ea564a80b945680a71b11d5f7006fcf91feaf
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-07-08 14:58:23 +0530

    Revert "Add js files"
    This reverts commit 51d3f2fd973cb9307a24cd62763f6bd1eff7e494.

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,20 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const dotenv_1 = require("dotenv");
-const dbConnect_1 = require("./lib/dbConnect");
-const compilerRoutes_1 = require("./routes/compilerRoutes");
-const userRoutes_1 = require("./routes/userRoutes");
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-app.use(express_1.default.json());
-app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ credentials: true, origin: "http://localhost:5173" }));
-(0, dotenv_1.config)();
-app.use('/compiler', compilerRoutes_1.compilerRoutes);
-app.use('/user', userRoutes_1.userRouter);
-(0, dbConnect_1.dbConnect)();
+app.get("/", (req, res) => {
+    return res.status(200).send("ok!");
+});
 app.listen(4000, () => {
     console.log(`http://localhost:4000`);
 });
